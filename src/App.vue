e29587a17adcdd0c24ff69be582f20889ee9d7ff
Baran <me@baran.wang>
2019-08-20 13:48:53 +0800

    feat: imco signature

--- src/App.vue
+++ src/App.vue
@@ -28,38 +28,20 @@ textarea {
     <form>
       <label>
         <span>Title</span>
-        <input type="text"
-               v-model="title">
+        <input type="text" v-model="title" />
       </label>
       <label>
         <span>Name</span>
-        <input type="text"
-               v-model="name">
+        <input type="text" v-model="name" />
       </label>
       <label>
         <span>Mobile</span>
-        <input type="text"
-               v-model="mobile">
-      </label>
-
-      <label>
-        <span>Base</span>
-        <select v-model="base">
-          <option v-for="item in cityList"
-                  :value="item"
-                  v-text="item"></option>
-        </select>
-      </label>
-
-      <label>
-        <span>Address</span>
-        <input type="text"
-               v-model="address">
+        <input type="text" v-model="mobile" />
       </label>
     </form>
-    <hr>
+    <hr />
     <div v-html="html"></div>
-    <hr>
+    <hr />
     <textarea v-text="html"></textarea>
   </div>
 </template>
@@ -67,16 +49,13 @@ textarea {
 <script>
 export default {
   data: () => ({
-    title: 'Designer',
-    name: 'Panda WANG',
-    mobile: '+86 175-2109-6154',
-    base: 'SHANGHAI',
-    address: '哈尔滨路 160 号老洋行 1913 创意园 A116-119',
-    cityList: ['SHANGHAI', 'BEIJING', 'CHENGDU', 'TAIPEI', 'HONGKONG', 'TOKYO']
+    title: "Designer",
+    name: "Panda WANG",
+    mobile: "+86 175-2109-6154"
   }),
   computed: {
-    html: function () {
-      return `<div style="font-family:Verdana,sans-serif;font-size:x-small;color:#333;"><div>${this.title}&nbsp;·&nbsp;<b>${this.name}</b></div><div>Mobile&nbsp;·&nbsp;<b>${this.mobile}</b></div><div>${this.base}&nbsp;·&nbsp;${this.address}</div><img src="https://pics.kanjian.com/kanjiancom/logo_emali_signature.png" style="width:176px;height:60px;"><div>${this.cityList.join('·')}</div><div>看见音乐&nbsp;<a href="http://www.kanjian.com"><b style="color:#0000ff">KANJIAN.COM</b></a></div><div>星球发行&nbsp;<a href="http://star.kanjian.com"><b style="color:#0000ff">STAR.KANJIAN.COM</b></a></div><div>国际版&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://en.kanjian.com"><b style="color:#0000ff">EN.KANJIAN.COM</b></a></div></div>`
+    html: function() {
+      return `<div style="font-family:Verdana,sans-serif;font-size:x-small;color:#333;"><div>${this.title}&nbsp;·&nbsp;<b>${this.name}</b></div><div>Mobile&nbsp;·&nbsp;<b>${this.mobile}</b></div><img src="https://static.imcochina.com/signature.png" style="width:260px;height:60px;"><div>Website&nbsp;·&nbsp;<a href="https://www.imcochina.com"><b style="color:#0000ff">IMCOCHINA.COM</b></a></div></div>`;
     }
   }
 };
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/kanjian-signature/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/imco-signature/" : "/"
 };
